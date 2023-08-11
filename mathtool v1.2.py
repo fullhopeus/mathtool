dba4249c34b6dff016332d67e9e58b2c1644aabb
Wang Peiyao <139528042+fullhopeus@users.noreply.github.com>
2023-08-11 08:23:27 +0100

    Update mathtool v1.2.py

--- mathtool v1.2.py
+++ mathtool v1.2.py
@@ -25,13 +25,11 @@ def factor(number): #直接调用
 def primefactors(n):
     factors = []
     divisor = 2
-
     while n > 1:
         while n % divisor == 0:
             factors.append(divisor)
             n //= divisor
         divisor += 1
-
     return factors
 
 def primefactor(n): #直接调用
